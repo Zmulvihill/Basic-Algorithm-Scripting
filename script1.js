173f8dfcde32067cd90fbaa39563bfdc95ddb6af
Zmulvihill <zmulvihi@yahoo.com>
2023-02-26 17:17:32 -0700

    added script1, didn't run node

---
+++ script1.js
@@ -0,0 +1,9 @@
+//Convert Celsius to Fahrenheit
+function convertCtoF(celsius) {
+  let fahrenheit = celsius * (9 / 5) + 32;
+  return fahrenheit;
+}
+
+// Change the inputs below to test your code
+convertCtoF(30);
+console.log(convertCtoF(30)
