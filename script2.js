23225328f6c35e6c6653340ce011548da6136c58
Zmulvihill <zmulvihi@yahoo.com>
2023-03-15 16:41:37 -0700

    added more to script2 and ran node

--- script2.js
+++ script2.js
@@ -1,8 +1,12 @@
 //Reverse a String
 function reverseString(str) {
-    let reversedStr = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-      reversedStr += str[i];
-    }
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
     return reversedStr;
   }
+  
+  console.log(reverseString("hello"))
+  console.log(reverseString("Howdy"))
+  console.log(reverseString("Greetings from Earth"))
